0ea2d21f702c84e5c36ab6542d5144e3d9e8f62d
Kevin Wu <kevinwu098@gmail.com>
2024-07-14 13:53:31 -0700

    feat: update server CORS

--- server/app.js
+++ server/app.js
@@ -2,28 +2,29 @@ const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser"); // FIXME: Keep only if using cookies
 const schedule = require("node-schedule"); // FIXME: Keep only if scheduling cronjobs
+require("dotenv").config();
 
 // Routes
 const sampleRouter = require("./routes/sample"); // FIXME: delete sample router
-
-require("dotenv").config();
 
 schedule.scheduleJob("0 0 0 0 0", () => console.log("Hello Cron Job!")); // FIXME: delete sample cronjob
 
 const app = express();
 
 const CLIENT_HOSTNAME =
-    process.env.NODE_ENV === "development"
+    process.env.NODE_ENV === "DEVELOPMENT"
         ? `${process.env.DEV_CLIENT_HOSTNAME}:${process.env.DEV_CLIENT_PORT}`
         : process.env.PROD_CLIENT_HOSTNAME;
+
 const SERVER_PORT =
-    (process.env.NODE_ENV === "development"
+    (process.env.NODE_ENV === "DEVELOPMENT"
         ? process.env.DEV_SERVER_PORT
         : process.env.PROD_SERVER_PORT) ?? 3001;
 
 app.use(
     cors({
         origin: CLIENT_HOSTNAME,
+        credentials: true,
     })
 );
 
