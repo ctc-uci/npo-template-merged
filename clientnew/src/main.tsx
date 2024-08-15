edd4cd12f4ee4d0d9b1d7fbabf0d95829c31678a
Kevin Wu <kevinwu098@gmail.com>
2024-08-14 20:59:33 -0700

    feat: update clientnew, update prettier

--- clientnew/src/main.tsx
+++ clientnew/src/main.tsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')!).render(
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+
+import App from "./App.tsx";
+
+import "./index.css";
+
+const colors = {
+  brand: {},
+};
+
+const theme = extendTheme({ colors });
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
+  </StrictMode>
+);
