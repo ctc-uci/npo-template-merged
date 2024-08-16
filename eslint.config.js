08dd2edbaa30a2f598cf4eabc48ee8add160f461
Kevin Wu <kevinwu098@gmail.com>
2024-08-16 00:16:54 -0700

    ci: add workflow, update eslint, husky, licences, root configs (#7)
    * chore: rename tsconfig node to tsconfig vite
    
    * ci: add workflow for running checks
    
    * conf: add nvmrc
    
    * ci: move github workflow to root, update with workspaces features
    
    * ci: correct command
    
    * ci: again
    
    * ci: remove usage of foreach
    
    * ci: remove usage of yarn workspace command
    
    * ci: fix dep install
    
    * ci: fix path?
    
    * ci: correct lint command?
    
    * ci: fix eslint
    
    * ci: remove outdated ci workflows
    
    * chore: move license to root
    
    * ci: update husky hook
    
    * chore: add PR template

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,4 @@
 import js from "@eslint/js";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 
@@ -12,16 +10,8 @@ export default tseslint.config({
     ecmaVersion: 2020,
     globals: globals.browser,
   },
-  plugins: {
-    "react-hooks": reactHooks,
-    "react-refresh": reactRefresh,
-  },
+
   rules: {
-    ...reactHooks.configs.recommended.rules,
-    "react-refresh/only-export-components": [
-      "warn",
-      { allowConstantExport: true },
-    ],
     eqeqeq: "error",
   },
 });
