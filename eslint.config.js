fc6e980c6a140381cf719e2d76f87b8dd3e0162a
Kevin Wu <kevinwu098@gmail.com>
2024-08-17 00:28:30 -0700

    chore: update configs

--- eslint.config.js
+++ eslint.config.js
@@ -4,13 +4,12 @@ import tseslint from "typescript-eslint";
 
 export default tseslint.config({
   extends: [js.configs.recommended, ...tseslint.configs.recommended],
-  files: ["src/**/*.{ts,tsx,js,jsx}"],
+  files: ["**/*.{ts,tsx,js,jsx}"],
   ignores: ["dist"],
   languageOptions: {
     ecmaVersion: 2020,
     globals: globals.browser,
   },
-
   rules: {
     eqeqeq: "error",
   },
