413a8b6b11dcc4aff4be4d81e74668eb5e2b4f40
Kevin Wu <kevinwu098@gmail.com>
2024-08-22 00:27:02 -0700

    feat: use concurrently

---
+++ concurrently.ts
@@ -0,0 +1,34 @@
+import path from "path";
+import { fileURLToPath } from "url";
+
+import concurrently from "concurrently";
+
+// Define __dirname for ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Define absolute paths for client and server
+const clientPath = path.resolve(__dirname, "client");
+const serverPath = path.resolve(__dirname, "server");
+
+const { result } = concurrently(
+  [
+    { command: `yarn run dev`, name: "client", cwd: clientPath },
+    { command: `yarn run start`, name: "server", cwd: serverPath },
+  ],
+  {
+    prefix: "name",
+    prefixColors: ["cyan", "magenta"],
+    killOthers: ["failure"],
+  }
+);
+
+function success() {
+  console.log("All processes completed successfully.");
+}
+
+function failure(err: unknown) {
+  console.error("An error occurred (all processes killed):", err);
+}
+
+result.then(success, failure);
