8403034da202270dcc36684aa619235b0c5cd68c
Kevin Wu <kevinwu098@gmail.com>
2024-08-26 00:10:08 -0700

    feat: use patch-package to fix firebase

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,11 +1,10 @@
-import basicSsl from "@vitejs/plugin-basic-ssl";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig(() => {
   return {
-    plugins: [react(), basicSsl()],
+    plugins: [react()],
     server: {
       proxy: {
         "/__/auth": {
