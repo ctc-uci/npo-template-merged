c236ff413e14c6cf926eb7256b894f8c3bef02f8
Kevin Wu <kwu@stokespace.com>
2024-08-27 22:08:19 -0700

    feat: basic data fetching, types directory

--- client/src/pages/DashboardPage.tsx
+++ client/src/pages/DashboardPage.tsx
@@ -1,18 +1,78 @@
-import { Button, Heading, VStack } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+
+import {
+  Button,
+  Heading,
+  Table,
+  TableCaption,
+  TableContainer,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+  VStack,
+} from "@chakra-ui/react";
 
 import { useAuthContext } from "../contexts/hooks/useAuthContext";
+import { useBackendContext } from "../contexts/hooks/useBackendContext";
+import { User } from "../types/users";
 
 export const DashboardPage = () => {
   const { logout } = useAuthContext();
+  const { backend } = useBackendContext();
+
+  const [users, setUsers] = useState<User[] | undefined>();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await backend.get("/users");
+        setUsers(response.data);
+      } catch (error) {
+        console.error("Error fetching users:", error);
+      }
+    };
+
+    fetchData();
+  }, [backend]);
 
   return (
     <VStack
       spacing={8}
-      sx={{ width: 300, marginX: "auto" }}
+      sx={{ maxWidth: "100%", marginX: "auto" }}
     >
       <Heading>Dashboard</Heading>
 
       <Button onClick={logout}>Sign out</Button>
+
+      <TableContainer
+        sx={{
+          overflowX: "auto",
+        }}
+      >
+        <Table variant="simple">
+          <TableCaption>Users</TableCaption>
+          <Thead>
+            <Tr>
+              <Th>Id</Th>
+              <Th>Email</Th>
+              <Th>FirebaseUid</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {users
+              ? users.map((user, i) => (
+                  <Tr key={i}>
+                    <Td>{user.id}</Td>
+                    <Td>{user.email}</Td>
+                    <Td>{user.firebaseUid}</Td>
+                  </Tr>
+                ))
+              : null}
+          </Tbody>
+        </Table>
+      </TableContainer>
     </VStack>
   );
 };
